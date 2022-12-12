9ea953dec34ec8669c00cd683bfaa4eac794330d
kadekillary <kadekillary@pm.me>
2022-12-12 03:59:23 -0800

    Update elixir ds are weirddddd

--- 7-d-no-space-left-on-device/nospace/lib/nospace.ex
+++ 7-d-no-space-left-on-device/nospace/lib/nospace.ex
@@ -21,5 +21,40 @@ defmodule Nospace do
     |> Enum.map(&String.trim/1)
     |> Enum.map(&String.replace(&1, "$ ", ""))
     |> Enum.filter(&(String.length(&1) > 0))
+    |> Enum.filter(&(&1 != "ls"))
+    # |> Enum.each(&IO.puts/1)
+    # |> Enum.each(&handle_token/1)
   end
+
+  # def build_tree(lst_tok) do
+    # # create a node from first token and add to tree
+    # tree = Tree.new(Node.new(List.last(String.split(List.first(lst_tok), " "))))
+    # tree.root
+    # |> IO.puts
+  # end
+  # def handle_token(lst_tok) do
+    # Enum.each(lst_tok, fn tok ->
+      # curr_node = nil
+      # case String.split(tok, " ") do
+        # # create root node
+        # ["cd", "/"] ->
+          # root_node = TreeNode.new("/")
+          # curr_node = root_node
+        # # go up a dir, change to curr node
+        # ["cd", ".."] ->
+          # curr_node = curr_node.parent
+        # # find child node, go down a dir
+        # ["cd", dir] ->
+          # curr_node = Enum.find(curr_node.children, &(&1.name == dir))
+        # # add new node w/ parent as curr node
+        # ["dir", name] ->
+          # new_node = TreeNode.new(name, curr_node)
+          # curr_node.children = [new_node | curr_node.children]
+        # # add filesize to current dir
+        # [filesize, _] ->
+          # curr_node.size + String.to_integer(filesize)
+        # _ -> IO.puts("error")
+      # end
+    # end)
+  # end
 end
